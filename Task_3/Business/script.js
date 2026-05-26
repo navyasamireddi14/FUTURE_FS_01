c56b4ef48d4aeecec38c8a732855f3b824b1e0dc
navyasamireddi14 <navyasamireddi14@gmail.com>
2026-05-26 16:10:32 +0530

    FS1

--- Task_3/Business/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("form").addEventListener("submit", function(event){
-
-    event.preventDefault();
-
-    alert("Thank you for contacting PowerFit Gym! We will get back to you soon.");
-
-    this.reset();
-
-});
